6510edba465250b98f0b0768323f6325a2f88bf0
Valentyn Kolesnikov <javadev75@gmail.com>
2018-11-06 09:30:47 +0100

    Add Implement_strstr

---
+++ src/main/java/N_Implement_strstr.java
@@ -0,0 +1,32 @@
+﻿public class N_Implement_strstr
+{
+    int strstr(String s, String x)
+    {
+        // if s is null or if s's length is less than that of x's
+        if (s == null || x.length() > s.length()) {
+            return -1;
+        }
+
+        // if x is null or is empty
+        if (x == null || x.length() == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= s.length() - x.length(); i++)
+        {
+            int j;
+            for (j = 0; j < x.length(); j++) {
+                if (x.charAt(j) != s.charAt(i + j)) {
+                    break;
+                }
+            }
+
+            if (j == x.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+}
